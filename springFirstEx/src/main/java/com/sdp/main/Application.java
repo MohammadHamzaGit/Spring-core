e5d6aafb818afe0b009d7545bcb09178fc764915
Mohammad Hamza Abbasi <abbasimohammadhamza29@gmail.com>
2026-06-29 22:16:53 +0530

    first Spring example enhanced

--- springFirstEx/src/main/java/com/sdp/main/Application.java
+++ springFirstEx/src/main/java/com/sdp/main/Application.java
@@ -15,7 +15,6 @@ class Application{
 
         MessageWriter messageWriter = (MessageWriter) beanFactory.getBean("messageWriter");
         MessageProducer messageProducer = (MessageProducer) beanFactory.getBean("htmlMessageProducer");
-        //MessageProducer PDFmessageProducer = (MessageProducer) beanFactory.getBean("pdfMessageProducer");
         messageWriter.setProducer(messageProducer);
         messageWriter.writeMessage("Welcome to Spring ");
     }
